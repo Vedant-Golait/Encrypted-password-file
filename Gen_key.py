23077742aef3b0aa78a74b9d12a9402a1cb6c965
Vedant Golait <vedantgolait04@gmail.com>
2024-05-24 10:56:46 +0530

    Delete Gen_key.py

--- Gen_key.py
+++
@@ -1,7 +0,0 @@
-from cryptography.fernet import Fernet
-
-# --------------------------To generate a Key---------------
-
-key =  Fernet.generate_key() #generates a key
-with open("code\mykey.key",'wb') as mykey: #writes the key to a file
-    mykey.write(key)
